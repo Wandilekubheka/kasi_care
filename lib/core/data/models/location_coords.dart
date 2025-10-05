b3984d9b70e86046028b84e3961b2479529ee13b
Wandile Kubheka <retroff01@gmail.com>
2025-10-05 17:32:54 +0200

    firebase included to project

--- lib/core/data/models/location_coords.dart
+++ lib/core/data/models/location_coords.dart
@@ -3,4 +3,15 @@ class LocationCoords {
   final double longitude;
 
   LocationCoords({required this.latitude, required this.longitude});
+
+  Map<String, dynamic> toJson() {
+    return {'latitude': latitude, 'longitude': longitude};
+  }
+
+  factory LocationCoords.fromJson(Map<String, dynamic> json) {
+    return LocationCoords(
+      latitude: json['latitude'] ?? 0.0,
+      longitude: json['longitude'] ?? 0.0,
+    );
+  }
 }
